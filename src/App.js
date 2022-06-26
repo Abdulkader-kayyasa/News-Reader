bfc9597d6f66e8811b097092c682a1cdc6388127
Abdul Kader Kayyasa <Abdulkader.kayasseh96@gmail.com>
2022-06-26 13:30:08 +0300

    deploy files

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React , {useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles';
+import logo from './img/covernews.jpg';
+import wordsToNumbers from 'words-to-numbers';
+const alanKey = 'fae9ea12a8cd64a8eaacb52c9b0bb3f02e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+    const [newsArticles , setNewsArticles] = useState([]);
+    const [activeArticle , setActiveArticle] = useState(-1);
+    const classes = useStyles() ;
+    useEffect(() => {
+        alanBtn(
+            { 
+                key: alanKey,
+                onCommand: ({ command, articles, number }) => {
+                     if (command === 'newHeadlines')
+                        {
+                          setNewsArticles(articles);
+                      
+                        }
+                        else if(command === 'highlight'){
+                            setActiveArticle((prevActiveArticle)=> prevActiveArticle + 1);
+                        }
+                        else if (command === 'open') {
+                            const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+                            const article = articles[parsedNumber - 1];
+                  
+                            if (parsedNumber > articles.length) {
+                              alanBtn().playText('Please try that again...');
+                            } else if (article) {
+                              window.open(article.url, '_blank');
+                              alanBtn().playText('Opening...');
+                            } else {
+                              alanBtn().playText('Please try that again...');
+                            }
+                          }
+            }
+        })
+    } , [])
+    return (
+        <div>
+            <div className={classes.logoContainer}>
+                <img src={logo} className={classes.alanLogo} alt="alan logo"></img>
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
+        </div>
+    );
+}
+
+export default App ;
